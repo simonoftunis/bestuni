1d4e9d98d1c5df5f3e84a87524460b65b65481fb
simonoftunis <khalilbenkhoud98@gmail.com>
2021-04-19 18:26:37 +0100

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+
+ function show()
+ {   
+    document.getElementsByClassName("menu")[0].style.display = "block" ;
+ }
+function hide()
+{
+     document.getElementsByClassName("menu")[0].style.display = "none" ;
+}
